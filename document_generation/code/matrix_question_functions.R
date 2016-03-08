da60b0ad42194760ef62653c7402d2b83b87e6ca
Christian Testa <testa101@gmail.com>
2016-03-08 16:49:50 -0500

    Adding some matrix question functions

--- document_generation/code/matrix_question_functions.R
+++ document_generation/code/matrix_question_functions.R
@@ -1,3 +1,24 @@
 is_matrix_question <- function(x) {
   return(x$Payload$QuestionType == "Matrix")
 }
+
+matrix_questions <- which(sapply(questions, is_matrix_question))
+
+matrix_get_respondents_count <- function(x) {
+  if (x$Payload$Selector == "Likert" &&
+  x$Payload$SubSelector == "SingleAnswer"){
+    respondents_count <- sapply(x$Responses, function(y) length(y != -99))
+    return(respondents_count)
+  } else if (x$Payload$Selector == "Likert" &&
+  x$Payload$SubSelector == "MultipleAnswer"){
+    respondents_count <- sapply(x$Responses, length)
+    return(respondents_count)
+  } else if (x$Payload$Selector == "Bipolar"){
+    respondents_count <- sapply(x$Responses, function(y) length(y != -99))
+    return(respondents_count)
+  } else if (x$Payload$Selector == "Likert" &&
+  x$Payload$SubSelector == "DL"){
+    respondents_count <- sapply(x$Responses, function(y) length(y != -99))
+    return(respondents_count)
+  }
+}
