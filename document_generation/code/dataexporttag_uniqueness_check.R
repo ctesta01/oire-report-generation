5dc0b2c45f9cbe3617cb1df0c80cbdaa545e2690
Christian Testa <testa101@gmail.com>
2016-03-15 15:39:46 -0400

    Improving the duplicate data export tag error

--- document_generation/code/dataexporttag_uniqueness_check.R
+++ document_generation/code/dataexporttag_uniqueness_check.R
@@ -1,5 +1,10 @@
 dataexporttags <- sapply(questions, function(x) x$Payload$DataExportTag)
 if (any(duplicated(dataexporttags))) {
-    stop("Duplicate Data Export Tags! Renumber the questions, please!")
-    quit()
+  stop(paste("There are questions with duplicated data export tags!
+  Please re-number the questions in Qualtrics,
+  redownload the QSF and CSV files,
+  and run the script with the new files.
+  The following data export tags are duplicated:",
+  toString(dataexporttags[which(duplicated(dataexporttags))]), sep=" "))
+  quit()
 }
